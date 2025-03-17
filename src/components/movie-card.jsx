db2e07bc0f4200bcee90831b181aa121038e1cfc
jazzm11 <jazzunciano1@gmail.com>
2025-03-17 17:30:50 +0100

    17-03-25

--- src/components/movie-card.jsx
+++ src/components/movie-card.jsx
@@ -2,7 +2,7 @@ import "/src/views/movie-card.css";
 
 function MovieCard(props) {
   return (
-    <div className="card-box">
+    <div className="card-box" src="/seriespage">
       <img src={props.image} id="card-img" />
       <div id="description">
         <h4>{props.name}</h4>
